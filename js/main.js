61aaeebafd71111842c76309a3e91dd4a73450c5
4BurnerStove <112003817+4BurnerStove@users.noreply.github.com>
2023-02-09 19:39:53 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -13,6 +13,8 @@ inputWeight.oninput = () => AlertError.close()
 submit.onsubmit = event => {
   event.preventDefault()
 
+  let IMCClassif = ''
+
   const Weight = inputWeight.value
   const Height = inputHeight.value
 
@@ -21,13 +23,27 @@ submit.onsubmit = event => {
   if (showAlertError) {
     AlertError.open()
     return
-  } 
+  }
 
   AlertError.close()
 
   const result = IMC(Weight, Height)
-  modal.wrapper.innerHTML = `O seu IMC é de ${result}`
+
+  if (result < 18.5) {
+    IMCClassif = 'Abaixo do peso'
+  } else if (result >= 18.5 && result < 25) {
+    IMCClassif = 'No peso normal'
+  } else if (result >= 25 && result < 30) {
+    IMCClassif = 'Sobrepeso'
+  } else if (result >= 30 && result < 35) {
+    IMCClassif = 'Obesidade Grau 1'
+  } else if (result >= 35 && result < 40) {
+    IMCClassif = 'Obesidade Grau 2'
+  } else {
+    IMCClassif = 'Obesidade Grau 3'
+  }
+
+  modal.wrapper.innerHTML = `O seu IMC é de ${result}, Você está ${IMCClassif}`
 
   modal.open()
 }
-
